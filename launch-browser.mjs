698eb91f0a4ac765fd5599551329aa007487556e
TheAtlasH <nomansible69@gmail.com>
2024-10-31 20:06:47 +0600

    assign done

--- launch-browser.mjs
+++ launch-browser.mjs
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer";
 async function main() {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"],
+    executablePath: executablePath(),
   });
   const version = await browser.version();
   console.log({ version });
